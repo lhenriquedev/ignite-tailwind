92b298f1fb3f9afc8007d392a3f99995c95b9346
Henrique <luiz@eway.dev>
2023-10-01 16:21:37 -0300

    adding radix-tabs and framer motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { SettingsTabs } from "@/components/SettingsTabs";
+
 export default function Home() {
-  return <h2>Hello tailwind</h2>;
+  return (
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+
+      <SettingsTabs />
+    </>
+  );
 }
