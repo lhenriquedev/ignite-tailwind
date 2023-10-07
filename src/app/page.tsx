fd1aadb11f4d9f6dffdce166f2ffe9b5e4a2a3eb
Henrique <luiz@eway.dev>
2023-10-07 19:47:27 -0300

    feat: adding files input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import * as FileInput from "@/components/Form/FileInput";
+
+import { InputControl, InputPrefix, InputRoot } from "@/components/Input";
+
+import { Mail } from "lucide-react";
 import { SettingsTabs } from "@/components/SettingsTabs";
 
 export default function Home() {
@@ -6,6 +11,171 @@ export default function Home() {
       <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
 
       <SettingsTabs />
+
+      <div className="mt-6 flex flex-col ">
+        <div className="flex items-center justify-between pb-5 border-b border-zinc-200">
+          <div className="space-y-1">
+            <h2 className="text-lg font-medium text-zinc-900">Personal info</h2>
+            <span className="text-sm text-zinc-500">
+              Update your photo and personal details here.
+            </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <button
+              type="button"
+              className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm border border-zinc-300 text-zinc-700 hover:bg-zinc-50"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              form="settings"
+              className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm bg-violet-600 text-white hover:bg-violet-700"
+            >
+              Save
+            </button>
+          </div>
+        </div>
+
+        <form
+          id="settings"
+          className="mt-6 flex flex-col gap-5 w-full divide-y divide-zinc-200"
+        >
+          <div className="grid gap-3 grid-cols-form">
+            <label
+              htmlFor="firstName"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Name
+            </label>
+            <div className="grid gap-6 grid-cols-2">
+              <InputRoot>
+                <InputControl id="firstName" defaultValue="Henrique" />
+              </InputRoot>
+
+              <InputRoot>
+                <InputControl defaultValue="Marques" />
+              </InputRoot>
+            </div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label
+              htmlFor="email"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Email address
+            </label>
+            <div className="grid gap-6 grid-cols-2">
+              <InputRoot>
+                <InputPrefix>
+                  <Mail className="w-5 h-5 text-zinc-500" />
+                </InputPrefix>
+                <InputControl
+                  type="email"
+                  id="email"
+                  defaultValue="Henrique@eway.dev"
+                />
+              </InputRoot>
+            </div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label
+              htmlFor="photo"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Your photo
+              <span className="text-sm mt-0.5 font-normal text-zinc-500 block">
+                This will be displayed on your profile.
+              </span>
+            </label>
+
+            <div>
+              <FileInput.Root className="flex items-start gap-5">
+                <FileInput.ImagePreview />
+                <FileInput.Trigger />
+                <FileInput.Control />
+              </FileInput.Root>
+            </div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label htmlFor="role" className="text-sm font-medium text-zinc-700">
+              Role
+            </label>
+            <div className="grid gap-6 grid-cols-2">
+              <InputRoot>
+                <InputControl type="text" id="role" defaultValue="CTO" />
+              </InputRoot>
+            </div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label
+              htmlFor="country"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Country
+            </label>
+            <div className="grid gap-6 grid-cols-2"></div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label
+              htmlFor="timezone"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Timezone
+            </label>
+            <div className="grid gap-6 grid-cols-2"></div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label htmlFor="bio" className="text-sm font-medium text-zinc-700">
+              Bio
+              <span className="text-sm mt-0.5 font-normal text-zinc-500 block">
+                Write a short introduction.
+              </span>
+            </label>
+            <div></div>
+          </div>
+
+          <div className="grid gap-3 grid-cols-form pt-5">
+            <label
+              htmlFor="projects"
+              className="text-sm font-medium text-zinc-700"
+            >
+              Portfolio projects
+              <span className="text-sm mt-0.5 font-normal text-zinc-500 block">
+                Share a few snippets of your work.
+              </span>
+            </label>
+            <div>
+              <FileInput.Root>
+                <FileInput.Trigger />
+                <FileInput.FileList />
+                <FileInput.Control multiple />
+              </FileInput.Root>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-end gap-2 pt-5">
+            <button
+              type="button"
+              className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm border border-zinc-300 text-zinc-700 hover:bg-zinc-50"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="rounded-lg px-4 py-2 text-sm font-semibold shadow-sm bg-violet-600 text-white hover:bg-violet-700"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
---
+++ src/components/Form/FileInput/Control.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { ChangeEvent, ComponentProps } from "react";
+
+import { useFileInput } from "./Root";
+
+interface ControlProps extends ComponentProps<"input"> {}
+
+export function Control(props: ControlProps) {
+  const { id, onFilesSelected } = useFileInput();
+
+  const handleFilesSelected = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files?.length) return;
+
+    const files = Array.from(event.target.files);
+    onFilesSelected(files);
+  };
+
+  return (
+    <input
+      type="file"
+      className="sr-only"
+      id={id}
+      {...props}
+      onChange={handleFilesSelected}
+    />
+  );
+}
